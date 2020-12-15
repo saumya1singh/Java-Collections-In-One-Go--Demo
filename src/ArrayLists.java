7a414b49c5be992cfdbccb16af18a387531b93b8
SaumyaSingh1 <ssaumyas100@gmail.com>
2020-12-15 11:49:56 +0530

    Adding Arraylists and it's  methods

--- src/ArrayLists.java
+++ src/ArrayLists.java
@@ -9,6 +9,8 @@ public class ArrayLists {
         arrayList.add(4F);
         arrayList.add(Float.valueOf(7));
         arrayList.add((float) 0.9);
+        arrayList.add(98.5F);
+        arrayList.add(7F);
 
         //size of list
         System.out.println("Size : " + arrayList.size());
@@ -26,9 +28,31 @@ public class ArrayLists {
         //remove from list
         System.out.println(arrayList.remove(2));
 
+        //get Index No from list
+        System.out.println(arrayList.indexOf(98.5F));
+
+        //get last index of a num from list
+        System.out.println(arrayList.lastIndexOf(7F));
+
+        //Create one more list and merge into previous list
+        ArrayList<Float> arrayList1= new ArrayList<>();
+        arrayList1.add(9F);
+        arrayList1.add(75F);
+        arrayList1.add(31F);
+
+        arrayList.addAll(arrayList1);
 
         //print list
-        System.out.println(arrayList);
+        System.out.println(arrayList.toString());
+
+        //Search for value at particular Index in list
+        System.out.println(arrayList.get(4));
+
+        //Print sublist within list
+        System.out.println(arrayList.subList(2, 5));
+        //2 - Inclusive and 5 - Exclusive
+
+
     }
 
 }
