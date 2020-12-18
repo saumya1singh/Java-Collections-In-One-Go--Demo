53348d502b43d7d023c6f56a69365755af249cfa
SaumyaSingh1 <ssaumyas100@gmail.com>
2020-12-18 19:38:59 +0530

    Adding Queue Class

---
+++ src/Queues.java
@@ -0,0 +1,32 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Queues {
+
+    public static void main(String[] args){
+
+        //Queue instance created using LinkedList
+        //It offers almost features of LinkedList
+
+        Queue<Integer> queue= new LinkedList<>();
+
+        queue.add(8);
+        queue.add(90);
+        queue.add(25);
+        queue.add(95);
+
+        System.out.println(queue.peek());
+
+        System.out.println(queue.poll());
+
+        System.out.println(queue.isEmpty());
+
+        System.out.println(queue.size());
+
+        System.out.println(queue.offer(97));
+
+        System.out.println(queue.toString());
+
+        System.out.println(queue);
+    }
+}
